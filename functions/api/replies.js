3b77ba551f0bc014222a56c628a3602f390e3490
Chris Philbin <chris@chrisphilbin.net>
2021-03-10 07:30:20 -0500

    Creating POST aciton to create new reply object

--- functions/api/replies.js
+++ functions/api/replies.js
@@ -22,3 +22,27 @@ exports.getAllReplies = (request, response) => {
 			return response.status(500).json({ error: err.code})
 		})
 }
+
+exports.createOneReply = (request, response) => {
+	if (request.body.message.trim() === '') {
+		return response.status(400).json({ message: 'Must not be empty' });
+    }
+        
+    const newReply = {
+        message: request.body.message,
+        rating: request.body.rating,
+        createdAt: new Date().toISOString()
+    }
+    db
+        .collection('replies')
+        .add(newReply)
+        .then((doc)=>{
+            const responseReply = newReply;
+            responseReply.id = doc.id;
+            return response.json(responseReply);
+        })
+        .catch((err) => {
+			response.status(500).json({ error: 'Something went wrong' });
+			console.error(err);
+		});
+};
--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,11 @@ const functions = require('firebase-functions')
 const app = require('express')()
 
 const {
-    getAllReplies
+    getAllReplies,
+    createOneReply
 } = require('./api/replies')
 
 app.get('/replies', getAllReplies)
+app.post('/replies', createOneReply)
 
 exports.api = functions.https.onRequest(app)
