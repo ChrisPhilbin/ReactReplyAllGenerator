a5ff03329d518bd4048cd52b99f039020cf14e36
Chris Philbin <chris@chrisphilbin.net>
2021-03-13 08:41:34 -0500

    Creating CreateReply component and setting up initial imports from @material-ui

---
+++ view/src/components/CreateReply.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState} from 'react'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import Select from '@material-ui/core/Select'
+import Slider from '@material-ui/core/Slider'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+
+const CreateReply = () => {
+    return(
+        <div>
+            Hello from CreateReply component
+        </div>
+    )
+}
+export default CreateReply
