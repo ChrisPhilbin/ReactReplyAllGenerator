a0fd71f29092c2d846d1f925e5f7b1bd8f9f7cad
Chris Philbin <chris@chrisphilbin.net>
2021-03-13 21:04:58 -0500

    Adding select element to display all type options when creating a new reply

--- view/src/components/CreateReply.js
+++ view/src/components/CreateReply.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState} from 'react'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+import Divider from '@material-ui/core/Divider'
 import { makeStyles } from '@material-ui/core/styles'
 import Select from '@material-ui/core/Select'
 import Slider from '@material-ui/core/Slider'
@@ -8,12 +9,31 @@ import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles((theme) => ({
-
+    divider: {
+        margin: 15
+    },
 }))
 
 const CreateReply = () => {
 
     let [types, setTypes] = useState([])
+    let [type, setType]   = useState('')
+    let [rating, setRating] = useState(5)
+
+    const marks = [
+        {
+            value: 2,
+            label: "No personality"
+        },
+        {
+            value: 5,
+            label: "Normal"
+        },
+        {
+            value: 9,
+            label: "Sold your soul"
+        }
+    ]
 
     useEffect(() => {
         fetch('/types')
@@ -23,10 +43,37 @@ const CreateReply = () => {
 
     const classes = useStyles()
 
+    const handleRatingChange = (event, newValue) => {
+        setRating(newValue)
+    }
+
     return(
         <Container maxWidth={sm}>
             <Paper elevation={3}>
                 <Typography align="center" variant="h3" gutterBottom>Add a new reply</Typography>
+                <Select
+                    fullWidth
+                    labelId="reply-type"
+                    id="reply-type"
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                >
+                    {types.map((type) => {
+                        <MenuItem value={type.name}>{type.name}</MenuItem>
+                    })}
+                </Select>
+                <Typography id="rating-slider" gutterBottom>Personality rating</Typography>
+                <Slider
+                    defaultValue={5}
+                    value={rating}
+                    onChange={handleRatingChange}
+                    aria-labelledby="discrete-slider-always"
+                    step={1}
+                    max={10}
+                    min={1}
+                    marks={marks}
+                    valueLabelDisplay="on"
+                />
             </Paper>
         </Container>
     )
