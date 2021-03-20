a2af36a63e831d2309fa72074709d54ec327dc87
Chris Philbin <chris@chrisphilbin.net>
2021-03-20 07:40:45 -0400

    Creating handleSubmit function to POST login data to API for authentication

--- view/src/pages/Login.js
+++ view/src/pages/Login.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import TextField from '@material-ui/core/TextField'
 
 const Login = () => {
+
+    let [username, setUsername] = useState('')
+    let [password, setPassword] = useState('')
+    let [error, setError]       = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        let userData = {
+            username: username,
+            password: password
+        }
+        fetch('/post', {
+            method: 'POST',
+            body: JSON.stringify(userData)
+        })
+        .then((response) => {
+            localStorage.setItem('AuthToken', `Bearer ${response.data.token}`);
+            props.history.push('/');
+        })
+        .catch(error => {
+            setError(error)
+            console.log(error, "something went wrong")
+        })
+    }
+
     return(
         <div>
             Hello from login!
