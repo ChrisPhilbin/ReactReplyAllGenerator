d23ce13400d44db3d2ef3df43f41183dde3baff9
Chris Philbin <chris@chrisphilbin.net>
2021-03-24 06:36:57 -0400

    Adding /signup route

--- view/src/App.js
+++ view/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Login from './pages/Login'
 import ReplySelector from './components/ReplySelector'
+import Signup from './pages/Signup'
 
 const App = () => {
 
@@ -9,8 +10,9 @@ const App = () => {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/"      component={ReplySelector} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/"       component={ReplySelector} />
+          <Route exact path="/login"  component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
       </div>
     </Router>
