489cd43c4411314a288620f871d7943061506527
Chris Philbin <chris@chrisphilbin.net>
2021-03-24 07:26:56 -0400

    Adding table to display list of all replies from API

--- view/src/App.js
+++ view/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Login from './pages/Login'
 import ReplySelector from './components/ReplySelector'
 import SignUp from './pages/Signup'
+import DisplayAllReplies from './components/DisplayAllReplies'
 
 const App = () => {
 
@@ -10,9 +11,10 @@ const App = () => {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/"       component={ReplySelector} />
-          <Route exact path="/login"  component={Login} />
-          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/"        component={ReplySelector} />
+          <Route exact path="/login"   component={Login} />
+          <Route exact path="/replies" component={DisplayAllReplies} />
+          <Route exact path="/signup"  component={SignUp} />
         </Switch>
       </div>
     </Router>
--- view/src/components/DisplayAllReplies.js
+++ view/src/components/DisplayAllReplies.js
@@ -1,6 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
-const DisplayAllReplies = () => {
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+    table: {
+        minWidth: 650
+    }
+}))
+
+const DisplayAllReplies = (props) => {
+
+    const classes = useStyles()
 
     let [replies, setReplies] = useState([])
     let [loading, setLoading] = useState(false)
@@ -8,10 +29,10 @@ const DisplayAllReplies = () => {
 
     useEffect(() => {
         setLoading(true)
-        fetch('/replies')
-        .then(response => response.json)
+        fetch(process.env.REACT_APP_CORS + '/replies')
+        .then(response => response.json())
         .then(data => {
-            setReplies(data)
+            setReplies([...replies, data])
             setLoading(false)
         })
         .catch(error => {
@@ -20,10 +41,42 @@ const DisplayAllReplies = () => {
         })
 
     }, [])
+
+    let replyTable = null
+
+    if (replies.length !== 0) {
+        replyTable = (
+            <>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Body of reply</TableCell>
+                            <TableCell align="right">Type of reply</TableCell>
+                            <TableCell align="right">Personality score</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {replies.map((reply) => (
+                            <TableRow key={reply.replyId}>
+                                <TableCell component="th" scope="row">
+                                    {reply.message}
+                                </TableCell>
+                                <TableCell align="right">{reply.type}</TableCell>
+                                <TableCell align="right">{reply.rating}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </>
+        )
+    }
+
     return(
-        <div>
-            Hello from DisplayAllReplies!
-        </div>
+        <main className={classes.content}>
+            {replies.length? {replyTable} : <div>Nothing yet...</div>}
+        </main>
     )
 }
 
